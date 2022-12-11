9989adc6b240b90971963812b02d7916574f052e
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-12-11 17:13:22 +0200

    Readme added

---
+++ README.md
@@ -0,0 +1,19 @@
+# similarity-metrics
+
+Library to compute event logs similarity metrics.
+
+## Examples
+
+```rust
+use similarity_metrics::string_distances::damerau_levenshtein_on_logs;
+
+let path_one = "filename_one.csv";
+let path_two = "filename_two.csv";
+let columns = &["concept:name", "org:resource", "start_timestamp", "time:timestamp"];
+
+let (distance, similarity) = damerau_levenshtein_on_logs(path_one, path_two, columns);
+```
+
+## Features
+
+* `damerau_levenshtein` - Compute the Damerau-Levenshtein distance between two event logs.
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
+//! Library to compute event logs similarity metrics.
+//!
+//! # Examples
+//!
+//! ```
+//! use similarity_metrics::string_distances::damerau_levenshtein_on_logs;
+//!
+//! let path_one = "filename_one.csv";
+//! let path_two = "filename_two.csv";
+//! let columns = &["concept:name", "org:resource", "start_timestamp", "time:timestamp"];
+//!
+//! let (distance, similarity) = damerau_levenshtein_on_logs(path_one, path_two, columns);
+//! ```
+//!
+//! # Features
+//!
+//! * `damerau_levenshtein` - Compute the Damerau-Levenshtein distance between two event logs.
+
 pub mod string_distances;
